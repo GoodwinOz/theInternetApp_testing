6c8f7891581040dd24727a6fda4feb1e14f6e738
Goodwin <bogdan411@rambler.ru>
2021-03-11 15:27:55 +0200

    git info updated

---
+++ test/app.test.js
@@ -0,0 +1,20 @@
+const webdriver = require('selenium-webdriver')
+
+//Test function
+function searchTitle() {
+    
+    //Open browser
+    let driver = new webdriver.Builder().forBrowser('chrome').build()
+
+    //Nav to url
+    driver.get('https://the-internet.herokuapp.com/').then(function() {
+        driver.getTitle().then(function(title) {
+            setTimeout(function() {
+                console.log(title)
+                driver.quit()
+            }, 5000)
+        })
+    })
+}
+
+searchTitle()
